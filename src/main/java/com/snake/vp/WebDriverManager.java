a7cd8072b8a6db2bfa35e47f84969ef6c59bec38
Frederik Rodriguez <frederikrodriguez@velocitypartners.net>
2016-11-18 11:51:11 -0500

    Adding SauceLabs Magic

--- src/main/java/com/snake/vp/WebDriverManager.java
+++ src/main/java/com/snake/vp/WebDriverManager.java
@@ -1,22 +1,43 @@
 package com.snake.vp;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class WebDriverManager {
 	private static WebDriverManager instance = null;
 
+	public static final String USERNAME = "Snake";
+	public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+	public static final String sauce = "20dd3db0-b571-47b4-a057-0701e3460695"; // Only
+																				// for
+																				// testing
+																				// purposes
+
+	public static final String URL = "https://" + USERNAME + ":" + sauce + "@ondemand.saucelabs.com:443/wd/hub";
+
 	private WebDriver driver;
+
 	protected WebDriverManager() {
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\bin\\ChromeDriver\\chromedriver.exe");
-		
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+		try {
+			DesiredCapabilities caps = DesiredCapabilities.chrome();
+			caps.setCapability("platform", "Windows XP");
+			caps.setCapability("version", "43.0");
+
+			driver = new RemoteWebDriver(new URL(URL), caps);
+
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 
 	public static WebDriverManager getInstance() {
@@ -29,20 +50,19 @@ public class WebDriverManager {
 	public WebDriver getDriver() {
 		return driver;
 	}
-	
+
 	/**
 	 * This method leaves the user into the beginning
 	 */
-	public void navigate(){
+	public void navigate() {
 		driver.get("http://computer-database.gatling.io/computers");
-		
+
 	}
 
 	public void navigateToPublicSite() {
 		navigate();
 		driver.findElement(By.cssSelector("#content > p > a")).click();
-		
+
 	}
-	
-	
+
 }
